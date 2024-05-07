b20cacd4356bc7727ff3d272ed4e6ac110c36692
languardian <C110156227@nkust.edu.tw>
2024-05-07 19:26:35 +0800

    first commit

--- src/components/manage_components/Record.js
+++ src/components/manage_components/Record.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Record.css';
 import CalImage from '../../pictures/Cal.png';
 import WaterImage from '../../pictures/Water.png';
@@ -19,16 +19,17 @@ function Record({record}) {
   const [C_remain,setC] = useState(record.calories_remain)
   const [W_remain,setW] = useState(record.water_remain)
   const [E_remain,setE] = useState(record.exercise_remain)
-  if (C_remain <= 0){
-    setC("已達標")
-  }
-  
-  if (W_remain <= 0){
-    setW("已達標")
-  }
-  if (E_remain <= 0){
-    setE("已達成")
-  }
+  useEffect(() => {
+    if (C_remain <= 0) {
+      setC("已達標");
+    }
+    if (W_remain <= 0) {
+      setW("已達標");
+    }
+    if (E_remain <= 0) {
+      setE("已達成");
+    }
+  }, []); // 只在組件初始化時執行一次
   return (
     <div className="record-wrapper">
       <h1 style={{textAlign:'center'}}>身體狀況記錄!</h1>
@@ -43,7 +44,12 @@ function Record({record}) {
               <span>目前攝取<span className="bold">{record.calories_now}</span> 卡</span>
             </div>
             <div className="calories-row">
-              <span>剩餘<span className="bold">{C_remain}</span> 卡</span>
+              {C_remain === '已達標' ? (
+                  <span className="bold" style={{fontSize: 30, color: 'green'}}>{C_remain}</span>
+              )
+              :(
+                <span>剩餘<span className="bold">{C_remain}</span> 卡</span>
+              )}
             </div>
           </div>
           <ImageWithText src={WaterImage} alt="水份" text="水份攝取" fontSize="20px" />
@@ -55,7 +61,12 @@ function Record({record}) {
               <span>目前飲用<span className="bold">{record.water_now}</span> 毫升</span>
             </div>
             <div className="water-row">
-              <span>剩餘<span className="bold">{W_remain}</span> 毫升</span>
+            {W_remain === '已達標' ? (
+                  <span className="bold" style={{fontSize: 30, color: 'green'}}>{W_remain}</span>
+              )
+              :(
+                <span>剩餘<span className="bold">{W_remain}</span> 卡</span>
+              )}
             </div>
           </div>
         </div>
@@ -69,7 +80,12 @@ function Record({record}) {
               <span>目前達到<span className="bold">{record.exercise_now}</span> MET</span>
             </div>
             <div className="exercise-row">
-              <span>剩餘<span className="bold">{E_remain}</span> MET</span>
+            {E_remain === '已達成' ? (
+                  <span className="bold" style={{fontSize: 30, color: 'green'}}>{E_remain}</span>
+              )
+              :(
+                <span>剩餘<span className="bold">{E_remain}</span> 卡</span>
+              )}
             </div>
           </div>
         </div>
